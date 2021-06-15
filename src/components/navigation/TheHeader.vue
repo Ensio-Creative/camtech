baf05209aac71f0fc09f8068413101704a2ba4f7
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2021-06-15 19:10:29 +0100

    Created the the header

---
+++ src/components/navigation/TheHeader.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="header-container">
+    <header class="the-header container">
+      <div class="nav-img">
+        <router-link to="/">
+          <img src="img/carmel-s-tekno-logo-215x53 1.svg" alt="">
+        </router-link>
+      </div>
+      <TheSideNavToggle @toggle="$emit('sidenavToggle')" />
+      <div class="navigation-items">
+        <ul class="nav-list">
+          <li
+            v-for="links in routes"
+            :key="links.url"
+            class="nav-item"
+          >
+            <router-link
+              class="mt-3"
+              :to="links.url"
+            >
+              {{ links.title }}
+            </router-link>
+          </li>
+        </ul>
+        <div class="search">
+          <img src="img/Vector.svg" alt="">
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import TheSideNavToggle from '@/components/navigation/TheSideNavToggle.vue'
+import routes from '@/data/url.js';
+export default {
+  name: 'TheHeader',
+  components: { TheSideNavToggle },
+  setup () {
+
+    return {
+      routes
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  width: 100%;
+  height: 88px;
+  background-color: #fff;
+  box-shadow: 0px 0px 4px 1px rgba(35, 51, 111, 0.24);
+}
+
+.the-header {
+  display: flex;
+  color: var(--base-color);
+  padding: 20px 18px;
+  justify-content:space-between;
+}
+
+.logo {
+  margin: 0 10px;
+  font-size: 1.3rem;
+}
+.nav-img img {
+  width: 221px;
+  height: 55px;
+}
+.spacer {
+  /* flex: 1; */
+  width: 129px;
+}
+
+.navigation-items {
+  display: none;
+}
+
+@media (min-width: 994px) {
+  .header-container {
+    position: fixed;
+    top: 0px;
+    z-index: 1000;
+    width: 100%;
+    height: 88px;
+    background-color: #fff;
+  }
+  .navigation-items {
+    display: flex;
+    width: 800px;
+    justify-content: space-between;
+  }
+  .header-container {
+  background-color: #fff;
+}
+}
+
+.nav-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  font-size: 18px;
+}
+
+.nav-item {
+  margin: 0 10px;
+}
+
+.nav-item a {
+  text-decoration: none;
+  font-size: 13px;
+  font-weight: 400;
+  padding: 0px 25px 43px 25px;
+  color: var(--base-color-dark);
+}
+
+.nav-item a:hover,
+.nav-item a:active,
+.nav-item a.router-link-active {
+  color: var(--base-color);
+  border-bottom: 2px solid var(--base-color);
+}
+</style>
---
+++ src/data/url.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: 'HOME',
+    url: '/'
+  },
+  {
+    title: 'SERVICES',
+    url: '/services'
+  },
+  {
+    title: 'SOLUTIONS',
+    url: '/solutions'
+  },
+  {
+    title: 'CONTACT US',
+    url: '/contact-us'
+  }
+]
